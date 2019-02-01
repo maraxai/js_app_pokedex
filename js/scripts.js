2b3ff7b37b94a9d816db6e2a6c74ae8e7a4c7b57
Mike Dietz <mikedietz@hotmail.com>
2019-02-01 19:22:33 +0100

    1.4 exchange For Loop with .forEach(), incl. IIFE and Repository Pattern

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,81 @@
-var repository = [
-  {
-    name : 'Bonsly',
-    number : '438',
-    height : 1.66,
-    weight : 33.1,
-    category : 'Bonsai',
-    abilities : ['Rock Head', 'Sturdy'],
-    type : ['Rock']
-  },
-  {
-    name : 'Probopass',
-    number : '476',
-    height : 4.58,
-    weight : 749.6,
-    category : 'Compass',
-    abilities : ['Sturdy', 'Magnet Pull'],
-    type : ['Rock', 'Steel']
-  },
-  {
-    name : 'Pikachu',
-    number : '025',
-    height : 1.33,
-    weight : 13.2,
-    category : 'Mouse',
-    abilities : ['Static'],
-    type : ['Electric']
-  },
-  {
-    name : 'Oddish',
-    number : '043',
-    height : 1.66,
-    weight : 11.9,
-    category : 'Weed',
-    abilities : ['Chlorophyll'],
-    type : ['Grass', 'Poison']
-  },
-  {
-    name : 'Digglet',
-    number : '050',
-    height : 0.66,
-    weight : 1.8,
-    category : 'Mole',
-    abilities : ['Sand Veil', 'Arena Trap'],
-    type : ['Ground']
-  }
-];
+var pokemonRepository = (function() {
 
-for (var i = 0; i < repository.length; i++) {
-  if (repository[i].height > 4.33) {
-    repository[i].name =  (repository[i].name + ' "Wow, that\'s big!"');
+  var repository = [
+    {
+      name : 'Bonsly',
+      number : '438',
+      height : 1.66,
+      weight : 33.1,
+      category : 'Bonsai',
+      abilities : ['Rock Head', 'Sturdy'],
+      type : ['Rock']
+    },
+
+    {
+      name : 'Probopass',
+      number : '476',
+      height : 4.58,
+      weight : 749.6,
+      category : 'Compass',
+      abilities : ['Sturdy', 'Magnet Pull'],
+      type : ['Rock', 'Steel']
+    },
+
+    {
+      name : 'Pikachu',
+      number : '025',
+      height : 1.33,
+      weight : 13.2,
+      category : 'Mouse',
+      abilities : ['Static'],
+      type : ['Electric']
+    },
+
+    {
+      name : 'Oddish',
+      number : '043',
+      height : 1.66,
+      weight : 11.9,
+      category : 'Weed',
+      abilities : ['Chlorophyll'],
+      type : ['Grass', 'Poison']
+    },
+
+    {
+      name : 'Digglet',
+      number : '050',
+      height : 0.66,
+      weight : 1.8,
+      category : 'Mole',
+      abilities : ['Sand Veil', 'Arena Trap'],
+      type : ['Ground']
+    }
+  ];
+
+  function add(element) {
+    repository.push();
   }
-  var name = ['<h2>' + repository[i].name + '</h2>'];
-  var pic = ['<img src="img/' + repository[i].number + '.png">'];
-  var features = ('<ul>' +
-    '<li>' + 'height: ' + repository[i].height +
-    '<li>' + 'weight: ' + repository[i].weight +
-    '<li>' + 'category: ' + repository[i].category +
-    '<li>' + 'abilities: ' + repository[i].abilities +
-    '<li>' + 'types: ' + repository[i].type + '</ul>');
-  document.write('<div>' + name + '<br>' + pic + '<br>' + features + '</div>');
-}
+
+  function getAll() {
+    return repository;
+  }
+
+  repository.forEach(function(element) {
+    if (element.height > 4.33) {
+      element.name =  (element.name + ' "Wow, that\'s big!"');
+    }
+    name = ['<h2>' + element.name + '</h2>'];
+    pic = ['<img src="img/' + element.number + '.png">'];
+    features = ('<ul>' +
+    '<li>' + 'height: ' + element.height +
+    '<li>' + 'weight: ' + element.weight +
+    '<li>' + 'category: ' + element.category +
+    '<li>' + 'abilities: ' + element.abilities +
+    '<li>' + 'types: ' + element.type + '</ul>');
+    document.write('<div>' + name + '<br>' + pic + '<br>' + features + '</div>');
+  });
+})();
+
+pokemonRepository.getAll(element).forEach(function(element) {
+  repository.addListItem(element);
+});
