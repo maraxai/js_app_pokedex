77d55d4bdec7df7937bfcd2afa534077d624f7b0
Mike Dietz <mikedietz@hotmail.com>
2019-02-10 23:01:46 +0100

    1.6 API

--- js/scripts.js
+++ js/scripts.js
@@ -1,103 +1,107 @@
 var pokemonRepository = (function() {
 
-  var pokemons = [
-    {
-      name : 'Bonsly',
-      number : '438',
-      height : 1.66,
-      weight : 33.1,
-      category : 'Bonsai',
-      abilities : ['Rock Head', 'Sturdy'],
-      type : ['Rock']
-    },
+  var pokemons = [];
 
-    {
-      name : 'Probopass',
-      number : '476',
-      height : 4.58,
-      weight : 749.6,
-      category : 'Compass',
-      abilities : ['Sturdy', 'Magnet Pull'],
-      type : ['Rock', 'Steel']
-    },
+  var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-    {
-      name : 'Pikachu',
-      number : '025',
-      height : 1.33,
-      weight : 13.2,
-      category : 'Mouse',
-      abilities : ['Static'],
-      type : ['Electric']
-    },
+  function addListItem(pokemon) {
+    var $ul = document.querySelector('ul');
+    var $li = document.createElement('li');
+    $ul.appendChild($li);
+    var $btn = document.createElement('button');
+    $btn.classList.add('listButton');
+    $li.appendChild($btn);
+    $btn.innerText = pokemon.name;
 
-    {
-      name : 'Oddish',
-      number : '043',
-      height : 1.66,
-      weight : 11.9,
-      category : 'Weed',
-      abilities : ['Chlorophyll'],
-      type : ['Grass', 'Poison']
-    },
-
-    {
-      name : 'Digglet',
-      number : '050',
-      height : 0.66,
-      weight : 1.8,
-      category : 'Mole',
-      abilities : ['Sand Veil', 'Arena Trap'],
-      type : ['Ground']
-    }
-  ];
-
-function addListItem(pokemon) {
-  var $ul = document.querySelector('ul');
-  var $li = document.createElement('li');
-  $ul.appendChild($li);
-  var $btn = document.createElement('button');
-  $btn.classList.add('listButton');
-  $li.appendChild($btn);
-  $btn.innerText = pokemon.name;
-  var $section = document.querySelector('section');
-  $div = document.createElement('div');
-  $div.classList.add('pokemonDetails');
-  $section.appendChild($div);
-  $btn.addEventListener('click', function (event) {
-    showDetails();
-  $div.innerHTML = 'name: ' + pokemon.name + '<br>' +
-    '<img src="img/'+ pokemon.number + '.png" >' + '<br>' +
-    'number: ' + pokemon.number + '<br>' +
-    'height: ' + pokemon.height + '<br>' +
-    'weight: ' + pokemon.weight + '<br>' +
-    'category: ' + pokemon.category + '<br>' +
-    'abilities: ' + pokemon.abilities + '<br>' +
-    'type: ' + pokemon.type ;
-    console.log(pokemon.name);
-  });
-}
-
-function showDetails(pokemon) {
-   console.log(pokemon);
-}
-
-  function add(pokemon) {
-    pokemons.push(pokemon);
+    // select the div for the PokemonDetails
+    var $PokemonDiv = document.querySelector('div');
+    //add a class name
+    $PokemonDiv.classList.add('pokemonDetails');
+    //onclick button will show details of selected Pokemon
+    $btn.addEventListener('click', function (event) {
+      showDetails(pokemon);
+    $PokemonDiv.innerHTML = 'name: ' + pokemon.name + '<br>' +
+    '<img src="' + pokemon.imageUrl + '" >' + '<br>' +
+      'height: ' + pokemon.height + ' inches'+'<br>' +
+      'type: ' + pokemon.types ;
+      console.log(pokemon.name);
+    });
   }
 
-  function getAll() {
-    return pokemons;
+  function showDetails(item) {
+    loadDetails(item);
+    console.log(item);
   }
 
-  return {
-    add: add,
-    getAll: getAll,
-    addListItem : addListItem
+    function add(item) {
+      pokemons.push(item);
     }
+
+    function getAll() {
+      return pokemons;
+    }
+
+    function loadList() {
+      return fetch(apiUrl).then(function (response) {
+        return response.json();
+      })
+        .then(function (json) {
+          json.results.forEach(function (item) {
+            var pokemon = {
+              name: item.name,
+              detailsUrl: item.url
+            };
+            add(pokemon);
+          });
+        })
+        .catch(function (e) {
+         console.error(e);
+       })
+    }
+
+    function loadDetails(item) {
+      var url = item.detailsUrl;
+      return fetch(url)
+        .then(function (response) {
+        return response.json();
+        })
+        .then(function (details) {
+          // Now we add the details to the item
+          item.name = details.name;
+          item.imageUrl = details.sprites.front_default;
+          item.height = details.height;
+          item.types = Object.keys(details.types);
+        })
+        .catch(function (e) {
+          console.error(e);
+        });
+    }
+
+   return {
+       add: add,
+       getAll: getAll,
+       //search: search,
+       loadList: loadList,
+       showDetails : showDetails,
+       loadDetails: loadDetails,
+       addListItem : addListItem
+   };
 })();
+
+pokemonRepository.loadList().then(function() {
+    pokemonRepository.getAll().forEach(function(pokemon){
+      pokemonRepository.addListItem(pokemon);
+    });
+});
+
+  //  return {
+    //  add: add,
+    //  getAll: getAll,
+  //    addListItem : addListItem
+  //    }
+/*})();*/
 /* this function is the gateway to the inner IIFE - for all records (getAll()) and each one of them (forEach())
 the method addListItem() is invoked*/
-pokemonRepository.getAll().forEach(function(pokemon) {
-  pokemonRepository.addListItem(pokemon);
-});
+//pokemonRepository.getAll().forEach(function(pokemon) {
+//  pokemonRepository.addListItem(pokemon);
+//});
